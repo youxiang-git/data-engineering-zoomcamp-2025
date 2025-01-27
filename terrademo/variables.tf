b0c4c571ddcfed1f0086b1962b590f2b12a439fa
youxiang-git <chaiyouxiang@gmail.com>
2025-01-27 20:55:54 +0800

    finish homework

---
+++ terrademo/variables.tf
@@ -0,0 +1,30 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my-creds.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "terraform-demo-449108"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "asia-southeast1"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "terraform-demo-449108-terra-bucket"
+}
+
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
